844789a06385371a9208e3e9a6be19ce97d1fc4a
Luis Gabriel Araújo <harrycorpmail@gmail.com>
2022-06-07 14:30:20 +0000

    Adding decToBin.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print("[MENU]")
+print("\n[MENU]")
 print("1 - FATORIAL")
 print("2 - FIBONACCI")
 print("3 - MMC")
+print("4 - DECIMAL PARA BINÁRIO")
 
 choose = int(input("\nEscolha uma opção: "))
 
@@ -11,5 +12,7 @@ elif choose == 2:
   __import__('fibonacci')
 elif choose == 3:
   __import__('mmc')
+elif choose == 4:
+  __import__("decToBin")
 else:
   print("\n[Opção inválida!]")
